e7290e61e4ea04a0e8308cd5346aa6ab03143753
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-02 01:16:38 +0900

    [TO-DO] database自体をソートしたい

--- FFMultiply/ViewController/OnlineRankingViewController.swift
+++ FFMultiply/ViewController/OnlineRankingViewController.swift
@@ -55,7 +55,7 @@ final class OnlineRankingViewController: UIViewController {
             let bscore = (b.value as AnyObject).object(forKey: "score") as! Int
             return ascore < bscore
         }
-        let loadScore: (FIRDataSnapshot) -> Void = { snapshot in
+        let loadSortScore: (FIRDataSnapshot) -> Void = { snapshot in
             guard let values = snapshot.value as? [String: Any] else {
                 return
             }
@@ -91,7 +91,8 @@ final class OnlineRankingViewController: UIViewController {
             self.tableView.reloadData()
         }
         
-        ref.child("scores").observe(.value, with: loadScore) { error in
+        
+        ref.child("scores").observe(.value, with: loadSortScore) { error in
             print(error.localizedDescription)
         }
     }
@@ -188,5 +189,43 @@ extension OnlineRankingViewController: DZNEmptyDataSetSource {
     
     func backgroundColor(forEmptyDataSet scrollView: UIScrollView!) -> UIColor! {
         return UIColor.clear
+    }
+}
+
+// MARK: - Debug
+extension OnlineRankingViewController {
+    let loadScore: (FIRDataSnapshot) -> Void = { snapshot in
+        guard let values = snapshot.value as? [String: Any] else {
+            return
+        }
+        print("debug -----")
+        print(values)
+        let rev = values.values.reversed()
+        let revKey = values.keys.reversed()
+        var sc = (rev[0] as AnyObject).object(forKey: "score") as! Int
+        var na = (rev[0] as AnyObject).object(forKey: "name") as! String
+        var r = 1
+        self.dataArray = []
+        self.dataArray.append((1, sc, na))
+        if revKey.first == self.device_id {
+            self.myPosition = 0
+            self.myRank = r
+            self.rankLabel.text = "Your Rank: \(self.myRank)"
+        }
+        for i in 1 ..< min(50, rev.count) {
+            let nextSc = (rev[i] as AnyObject).object(forKey: "score") as! Int
+            na = (rev[i] as AnyObject).object(forKey: "name") as! String
+            if sc != nextSc {
+                r = i + 1
+                sc = nextSc
+            }
+            if revKey[i] == self.device_id {
+                self.myPosition = i
+                self.myRank = r
+                self.rankLabel.text = "Your Rank: \(self.myRank)"
+            }
+            self.dataArray.append((r, sc, na))
+        }
+        self.tableView.reloadData()
     }
 }
